47553c765460fd8e8d787fb7e5d27468be7937ae
aldogioia <149803224+aldogioia@users.noreply.github.com>
2025-03-16 02:54:01 +0100

    First commit

---
+++ src/main/java/org/aldo/beautycenter/BeautyCenterApplication.java
@@ -0,0 +1,13 @@
+package org.aldo.beautycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautyCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aldo/beautycenter/BeautyCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aldo.beautycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
