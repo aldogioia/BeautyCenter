fa31874a55393276276732fe4c5d0953ef095e10
aldogioia <149803224+aldogioia@users.noreply.github.com>
2025-03-18 03:01:37 +0100

    implementato rate limiting

--- src/main/java/org/aldo/beautycenter/controller/BookingController.java
+++ src/main/java/org/aldo/beautycenter/controller/BookingController.java
@@ -3,6 +3,7 @@ package org.aldo.beautycenter.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.aldo.beautycenter.data.dto.BookingDto;
+import org.aldo.beautycenter.security.availability.RateLimit;
 import org.aldo.beautycenter.security.customAnnotation.ValidBookingId;
 import org.aldo.beautycenter.security.customAnnotation.ValidUserId;
 import org.aldo.beautycenter.service.interfaces.BookingService;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@RateLimit(permitsPerSecond = 10)
 @RestController
 @RequestMapping("/api/v1/booking")
 @CrossOrigin(origins = "*")
