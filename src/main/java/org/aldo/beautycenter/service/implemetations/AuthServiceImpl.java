eeeac0346e46709d4e2843d78a7ea29fce66690a
aldogioia <149803224+aldogioia@users.noreply.github.com>
2025-04-19 14:30:22 +0200

    Aggiunto il meccanismo di Refresh Token

--- src/main/java/org/aldo/beautycenter/service/implemetations/AuthServiceImpl.java
+++ src/main/java/org/aldo/beautycenter/service/implemetations/AuthServiceImpl.java
@@ -2,16 +2,22 @@ package org.aldo.beautycenter.service.implemetations;
 
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.aldo.beautycenter.data.dao.UserDao;
 import org.aldo.beautycenter.data.dto.CreateCustomerDto;
+import org.aldo.beautycenter.data.entities.User;
+import org.aldo.beautycenter.data.enumerators.Token;
 import org.aldo.beautycenter.security.authentication.JwtHandler;
+import org.aldo.beautycenter.security.exception.customException.TokenException;
 import org.aldo.beautycenter.service.interfaces.AuthService;
 import org.aldo.beautycenter.service.interfaces.BlacklistService;
 import org.aldo.beautycenter.service.interfaces.CustomerService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -22,12 +28,44 @@ public class AuthServiceImpl implements AuthService {
     private final CustomerService customerService;
     private final AuthenticationManager authenticationManager;
     @Override
-    public String signIn(String email, String password) {
+    public Map<String, String> signIn(String email, String password) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(email, password));
 
-        return jwtHandler.generateToken(userDao.findByEmail(email)
-                .orElseThrow(() -> new EntityNotFoundException("Utente non trovato")));
+        User user = userDao.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("Utente non trovato"));
+
+        String accessToken = jwtHandler.generateAccessToken(user);
+        String refreshToken = jwtHandler.generateRefreshToken(user);
+
+        return Map.of(
+                "accessToken", accessToken,
+                "refreshToken", refreshToken
+        );
+    }
+
+    @Override
+    public String refresh(HttpServletRequest request) {
+        String refreshToken = jwtHandler.getJwtFromRequest(request, Token.REFRESH);
+
+        if (blacklistService.isTokenBlacklisted(refreshToken)) {
+            throw new TokenException("Refresh token fornito già revocato");
+        }
+
+        if (!jwtHandler.isValidRefreshToken(refreshToken)) {
+            throw new TokenException("Refresh token non valido o scaduto");
+        }
+
+        try {
+            String email = jwtHandler.getEmailFromToken(refreshToken);
+            var user = userDao.findByEmail(email)
+                    .orElseThrow(() -> new EntityNotFoundException("Utente non trovato"));
+
+            return jwtHandler.generateAccessToken(user);
+        }
+        catch (Exception e) {
+            throw new TokenException("Errore durante il refresh del token");
+        }
     }
 
     @Override
@@ -36,7 +74,9 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
+    @Transactional
     public void signOut(HttpServletRequest request) {
-        blacklistService.addTokenToBlacklist(jwtHandler.getJwtFromRequest(request));
+        blacklistService.addTokenToBlacklist(jwtHandler.getJwtFromRequest(request, Token.ACCESS));
+        blacklistService.addTokenToBlacklist(jwtHandler.getJwtFromRequest(request, Token.REFRESH));
     }
 }
