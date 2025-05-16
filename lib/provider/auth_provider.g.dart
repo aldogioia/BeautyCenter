42cc0544f8f7e7894b7ff19e431173b98f5adadd
FrancescoCavaliere11 <francescounical@gmail.com>
2025-05-16 09:31:15 +0200

    pushato auth_provider.g.dart

---
+++ lib/provider/auth_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authHash() => r'66710edfaa2937058265480e01293401274d54b8';
+
+/// See also [Auth].
+@ProviderFor(Auth)
+final authProvider =
+    AutoDisposeNotifierProvider<Auth, bool>.internal(
+  Auth.new,
+  name: r'authProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$authHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Auth = AutoDisposeNotifier<bool>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
